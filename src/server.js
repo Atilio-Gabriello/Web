0586c7d6731912a5147fe46b4747723c0827e6f0
Atilio-Gabriello <atilioga22@gmail.com>
2021-03-27 15:34:43 -0300

    Update server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 const express = require("express")
 const server = express()
 
+//Configurar pasta publica
+server.use(express.static("public"))
+
 server.get("/", (req,res) => {
-    res.send("Hello word")
+    res.sendFile(__dirname + "/views/index.html")
 })
 
+server.get("/create_point", (req,res) => {
+    res.sendFile(__dirname + "/views/create_point.html")
+})
+
+server.get("/search_results", (req,res) => {
+    res.sendFile(__dirname + "/views/search_results.html")
+})
+
+//Usado para ligar o servidor
 server.listen(3000)
