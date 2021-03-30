6265916e55c5402fb15deb019fb4ead1665c026d
Atilio-Gabriello <atilioga22@gmail.com>
2021-03-30 13:00:08 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ const sqlite3 = require("sqlite3").verbose()
 const db = new sqlite3.Database("./src/database/database.db")
 
 module.exports = db
-/*
+
 db.serialize(() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS places (
@@ -61,11 +61,11 @@ db.serialize(() => {
     })
 
     //FUNÇÃO PARA DELETAR REGISTROS
-    db.run(`DELETE FROM places WHERE id = ?`, [25], function(err){
+    db.run(`DELETE FROM places WHERE id = ?`, [31], function(err){
         if(err){
             return console.log(err)
         }
 
         console.log("Registro deletado")
     })
-})*/
+})
